e8c6a294f20b675604bb1e4b1e69e7af185c5510
Andy-Python-Programmer <Andy-Python-Programmer@users.noreply.github.com>
2021-03-28 04:29:57 +0000

    deploy: 59ec1e1b7e1564b35c83089f45b474bee6f70dfd

--- aero/drivers/ahci/sidebar-items.js
+++ aero/drivers/ahci/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["AHCI",""]]});
+initSidebarItems({"constant":[["HBA_PORT_DEV_PRESENT",""],["HBA_PORT_IPM_ACTIVE",""],["SATA_SIG_ATA",""],["SATA_SIG_ATAPI",""],["SATA_SIG_PM",""],["SATA_SIG_SEMB",""]],"enum":[["AHCIPortType",""]],"struct":[["AHCI",""],["HBAMemory",""],["HBAPort",""]]});
