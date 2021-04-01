ee43abb127556f15e36be1d59d2917c9b5877e29
Andy-Python-Programmer <Andy-Python-Programmer@users.noreply.github.com>
2021-04-01 01:13:58 +0000

    deploy: 31e3065abb0516455a0a06ec2eec3f7bac8bac15

--- aero/arch/x86_64/interrupts/exceptions/sidebar-items.js
+++ aero/arch/x86_64/interrupts/exceptions/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["bound_range",""],["breakpoint",""],["debug",""],["device_not_available",""],["divide_by_zero",""],["double_fault",""],["invalid_opcode",""],["invalid_tss",""],["non_maskable",""],["overflow",""],["page_fault",""]]});
+initSidebarItems({"fn":[["bound_range",""],["breakpoint",""],["debug",""],["device_not_available",""],["divide_by_zero",""],["double_fault",""],["invalid_opcode",""],["invalid_tss",""],["non_maskable",""],["overflow",""],["page_fault",""]],"struct":[["InterruptStack",""]]});
