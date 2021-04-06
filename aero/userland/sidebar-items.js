766847585ab6eceeba63e6fab66e2969a243ba49
Andy-Python-Programmer <Andy-Python-Programmer@users.noreply.github.com>
2021-04-06 04:00:38 +0000

    deploy: 55f890bb7caae7700312f6b6febf36c6420ea2e2

--- aero/userland/sidebar-items.js
+++ aero/userland/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["init","Initialize userland."],["jump_to_userland","Jump to userland."]],"mod":[["elf","ELF (Executable and Linkable Format) file parsing."],["process",""],["scheduler",""]]});
+initSidebarItems({"fn":[["init","Initialize userland."],["jump_userland",""]],"mod":[["elf","ELF (Executable and Linkable Format) file parsing."],["process",""],["scheduler",""]]});
