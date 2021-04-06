ae3d003d24d52dfcb00c62a9c2275cd745f391eb
Andy-Python-Programmer <Andy-Python-Programmer@users.noreply.github.com>
2021-04-06 00:51:01 +0000

    deploy: 5a2554d607ef02e510b1df11ed8b818e82cb0298

--- aero/arch/x86_64/sidebar-items.js
+++ aero/arch/x86_64/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["cpu",""],["elf",""],["gdt","The GDT contains entries telling the CPU about memory segments."],["interrupts",""],["memory",""],["tss","The Task State Segment (TSS) is a special data structure for x86 processors which holds information about a task."]]});
+initSidebarItems({"mod":[["cpu",""],["elf",""],["gdt","The GDT contains entries telling the CPU about memory segments."],["interrupts",""],["memory",""]]});
