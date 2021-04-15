b36ddaac2df0b618611e843db7bba7b8252fa532
Andy-Python-Programmer <Andy-Python-Programmer@users.noreply.github.com>
2021-04-15 00:34:42 +0000

    deploy: bdb82437e1d7388f560f0d3e72b4a0c8b5ddfb2c

--- aero_boot/sidebar-items.js
+++ aero_boot/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["MemoryRegionType","Represents the different types of memory."],["PixelFormat","Color format of pixels in the framebuffer."]],"fn":[["test_runner",""]],"struct":[["FrameBufferInfo","Describes the layout and pixel format of a framebuffer."]]});
+initSidebarItems({"enum":[["MemoryRegionType","Represents the different types of memory."],["PixelFormat","Color format of pixels in the framebuffer."]],"fn":[["test_runner",""]],"struct":[["BootInfo",""],["FrameBufferInfo","Describes the layout and pixel format of a framebuffer."]]});
