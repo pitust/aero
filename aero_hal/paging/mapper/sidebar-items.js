d7f7ff2710bc6d5465682689e6b1605d3df70698
Andy-Python-Programmer <Andy-Python-Programmer@users.noreply.github.com>
2021-04-17 08:24:30 +0000

    deploy: 8f7815afbf34e478596589da7738c300703474fc

--- aero_hal/paging/mapper/sidebar-items.js
+++ aero_hal/paging/mapper/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["MapperError","This error is returned from `map_to` and similar methods."]],"struct":[["Mapper",""],["MapperFlush",""]]});
+initSidebarItems({"enum":[["MapperError","This error is returned from `map_to` and similar methods."]],"struct":[["Mapper",""],["MapperFlush",""]],"trait":[["MapperMap",""]]});
