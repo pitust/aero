67e4d4e4deedfa34453e72c737deb7dac1e4f6ef
Andy-Python-Programmer <Andy-Python-Programmer@users.noreply.github.com>
2021-04-22 06:43:44 +0000

    deploy: 531bd15579dca5a937a0216f582b144150e2b101

--- aero_kernel/arch/x86_64/sidebar-items.js
+++ aero_kernel/arch/x86_64/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["cpu",""],["elf",""],["gdt","The GDT contains entries telling the CPU about memory segments."],["interrupts",""],["memory",""]]});
+initSidebarItems({"mod":[["elf",""],["gdt","The GDT contains entries telling the CPU about memory segments."],["interrupts",""],["memory",""]]});
