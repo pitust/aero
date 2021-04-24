6fe1179ce6b8bef98ab501cf9930e50dab3fe583
Andy-Python-Programmer <Andy-Python-Programmer@users.noreply.github.com>
2021-04-24 02:17:40 +0000

    deploy: 54ca546219812c3c3770b96c6ac5c102a490e70b

--- aero_kernel/syscall/sidebar-items.js
+++ aero_kernel/syscall/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["SYSCALL_TABLE_LENGTH",""]],"fn":[["init",""],["syscall_handler",""]],"mod":[["fs",""],["process",""],["time",""]],"static":[["SYSCALL_HANDLER_TABLE",""]],"struct":[["SyscallEntry",""],["SyscallTable",""]]});
+initSidebarItems({"constant":[["SYSCALL_TABLE_LENGTH",""],["SYS_EXIT",""]],"fn":[["init",""],["sys_unimplemented",""],["syscall_handler",""]],"mod":[["fs",""],["process",""],["time",""]],"static":[["SYSCALL_HANDLER_TABLE",""]],"struct":[["SyscallEntry",""],["SyscallTable",""]]});
