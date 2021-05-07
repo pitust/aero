6946f96aa0d43dc5f8bdc6cf79871fcbd7c9553a
Andy-Python-Programmer <Andy-Python-Programmer@users.noreply.github.com>
2021-05-07 08:46:37 +0000

    deploy: 423651baf90219ff45d77c727abf96ee2b43bfb8

--- doc/aero_kernel/tls/sidebar-items.js
+++ doc/aero_kernel/tls/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["init","Initialize support for the `#[thread_local]` attribute."]]});
+initSidebarItems({"fn":[["init","Initialize support for the `#[thread_local]` attribute."]],"static":[["THREAD_LOCAL_STORAGE",""]],"struct":[["ThreadControlBlock","The TCB (Thread Control Block) containg the self pointer to itself and that means the size of TCB will always be the size of a pointer."],["ThreadLocalStorage",""]]});
