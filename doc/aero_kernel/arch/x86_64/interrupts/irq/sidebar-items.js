ca8b2426d02fd9a6fa2c6ec2b63e6ba98f825907
Andy-Python-Programmer <Andy-Python-Programmer@users.noreply.github.com>
2021-05-08 06:58:30 +0000

    deploy: f9d2a1c28506c006f086c96cbad9d4ee8aa1439f

--- doc/aero_kernel/arch/x86_64/interrupts/irq/sidebar-items.js
+++ doc/aero_kernel/arch/x86_64/interrupts/irq/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["__interrupt_keyboard",""],["__interrupt_lapic_error",""],["__interrupt_mouse",""],["__interrupt_pit",""],["keyboard",""],["lapic_error",""],["mouse",""],["pit",""]]});
+initSidebarItems({"fn":[["keyboard",""],["lapic_error",""],["mouse",""],["pit",""]]});
