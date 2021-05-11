4cd747634be606be6c5d619e3abb769bc4f1165d
Andy-Python-Programmer <Andy-Python-Programmer@users.noreply.github.com>
2021-05-11 04:25:01 +0000

    deploy: 733fc8b83bd0e7b700ece4577649131499c897dc

---
+++ doc/aero_kernel/fs/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["init","Initialize the file system. By default aero will use Aero VFS for its file system."]],"mod":[["vfs","A Virtual File System (VFS) is just an abstraction that many operating systems provide to applications."]],"trait":[["FileSystem","File systems are the machine’s way of ordering your data on readable and/or writable media."]]});
