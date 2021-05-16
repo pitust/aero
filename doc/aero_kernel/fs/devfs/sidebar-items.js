5a9c3344458c1f2f3169612ebf4f8bb1f730dafe
Andy-Python-Programmer <Andy-Python-Programmer@users.noreply.github.com>
2021-05-16 06:26:44 +0000

    deploy: 4a9f2614d4b171ba849f79d3a52daf6bf3488ee6

--- doc/aero_kernel/fs/devfs/sidebar-items.js
+++ doc/aero_kernel/fs/devfs/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["MAX_DEVICES",""]],"fn":[["init","Initialize devfs and install it in the dyn filesystem hashmap."],["install_device",""]],"struct":[["DEVICES",""],["DevFs",""],["DevNull",""],["DevUrandom",""],["DevZero",""]],"trait":[["Device",""]]});
+initSidebarItems({"fn":[["init","Initialize devfs and install it in the dyn filesystem hashmap."],["install_device",""]],"macro":[["impl_dev",""]],"static":[["DEVICES",""]],"struct":[["DevFs",""],["DevNull",""],["DevZero",""]],"trait":[["Device",""]]});
