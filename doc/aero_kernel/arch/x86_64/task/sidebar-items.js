e80d58a271603d107389beb68e1d54151e037d64
Andy-Python-Programmer <Andy-Python-Programmer@users.noreply.github.com>
2021-07-15 08:44:49 +0000

    deploy: e98e42c99ff961e96442431183bcf36044cc4db7

--- doc/aero_kernel/arch/x86_64/task/sidebar-items.js
+++ doc/aero_kernel/arch/x86_64/task/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["arch_switch",""]],"struct":[["ArchTask",""],["InterruptFrame",""],["KernelTaskFrame",""]]});
+initSidebarItems({"fn":[["arch_switch","This function is responsible for performing the inner task switch. Firstly it sets the new RSP in the TSS and then performes the actual context switch (saving the previous tasks state in its context and then switching to the new task)."]],"struct":[["ArchTask",""],["InterruptFrame",""],["KernelTaskFrame",""]]});
