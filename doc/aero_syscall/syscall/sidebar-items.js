2f82940c0aaaee9a9e7d0fa226ca0f870bc32eac
Andy-Python-Programmer <Andy-Python-Programmer@users.noreply.github.com>
2021-08-31 03:56:50 +0000

    deploy: 412093c002a9b2b4fa23ebcb7efd5239af88d3ca

--- doc/aero_syscall/syscall/sidebar-items.js
+++ doc/aero_syscall/syscall/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["supports_syscall_sysret","Returns true if the current CPU supports the `syscall` and the `sysret` instruction."],["syscall0",""],["syscall1",""],["syscall2",""],["syscall3",""],["syscall4",""],["syscall5",""],["syscall6",""]],"macro":[["define_syscall_fns",""]]});
+initSidebarItems({"fn":[["supports_syscall_sysret","Returns true if the current CPU supports the `syscall` and the `sysret` instruction."],["syscall0",""],["syscall1",""],["syscall2",""],["syscall3",""],["syscall4",""],["syscall5",""],["syscall6",""]]});
