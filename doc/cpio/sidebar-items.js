fe1f3f2ba7d1cac5b27a6fe215dd87b5c7855f77
Andy-Python-Programmer <Andy-Python-Programmer@users.noreply.github.com>
2021-09-16 04:22:28 +0000

    deploy: c577476693ac0150821f21d60a642acf7e324f67

--- doc/cpio/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"enum":[["ReadError","The error type which is returned from CPIO reader."]],"struct":[["CpioNewcReader","A CPIO file (newc format) reader."],["Metadata","The file metadata."],["Object","File system object in CPIO file."]]});
