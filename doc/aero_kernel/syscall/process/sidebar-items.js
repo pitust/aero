fb89c925d03e37999289bf8f0bdfb07a63f5e628
Andy-Python-Programmer <Andy-Python-Programmer@users.noreply.github.com>
2021-10-22 00:38:02 +0000

    deploy: d05155b1f9ab54f4f24d9a7b7f6956056544bba8

--- doc/aero_kernel/syscall/process/sidebar-items.js
+++ doc/aero_kernel/syscall/process/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["ARCH_GET_FS",""],["ARCH_GET_GS",""],["ARCH_SET_FS",""],["ARCH_SET_GS",""]],"fn":[["arch_prctl",""],["exit",""],["fork",""],["mmap",""],["munmap",""],["shutdown",""]]});
+initSidebarItems({"constant":[["ARCH_GET_FS",""],["ARCH_GET_GS",""],["ARCH_SET_FS",""],["ARCH_SET_GS",""]],"fn":[["arch_prctl",""],["exec",""],["exit",""],["fork",""],["log",""],["mmap",""],["munmap",""],["shutdown",""]]});
