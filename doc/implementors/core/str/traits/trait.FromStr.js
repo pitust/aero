745c67ddd146e3526bcaa559bacc678209b3ac48
Andy-Python-Programmer <Andy-Python-Programmer@users.noreply.github.com>
2021-11-13 09:13:36 +0000

    deploy: 5f94b4980b918ad40e57e7cc05de4cba038706c9

--- doc/implementors/core/str/traits/trait.FromStr.js
+++ doc/implementors/core/str/traits/trait.FromStr.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
+implementors["arrayvec"] = [{"text":"impl&lt;const CAP:&nbsp;usize&gt; FromStr for <a class=\"struct\" href=\"arrayvec/struct.ArrayString.html\" title=\"struct arrayvec::ArrayString\">ArrayString</a>&lt;CAP&gt;","synthetic":false,"types":["arrayvec::array_string::ArrayString"]}];
 implementors["log"] = [{"text":"impl FromStr for <a class=\"enum\" href=\"log/enum.Level.html\" title=\"enum log::Level\">Level</a>","synthetic":false,"types":["log::Level"]},{"text":"impl FromStr for <a class=\"enum\" href=\"log/enum.LevelFilter.html\" title=\"enum log::LevelFilter\">LevelFilter</a>","synthetic":false,"types":["log::LevelFilter"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
