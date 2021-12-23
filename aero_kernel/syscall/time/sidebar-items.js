f66f2a34391d1dc3284c192caebc6b5d71daf7e1
Andy-Python-Programmer <Andy-Python-Programmer@users.noreply.github.com>
2021-12-23 03:28:42 +0000

    deploy: abbe5a68474d2f106843f5997ce51c19884430a0

--- aero_kernel/syscall/time/sidebar-items.js
+++ aero_kernel/syscall/time/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["CLOCK_TYPE_MONOTONIC",""],["CLOCK_TYPE_REALTIME",""]],"fn":[["gettime",""]]});
+initSidebarItems({"constant":[["CLOCK_TYPE_MONOTONIC",""],["CLOCK_TYPE_REALTIME",""]],"fn":[["gettime",""],["sleep",""]]});
