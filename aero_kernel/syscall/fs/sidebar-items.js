07fbc10540d6253d97a264517ffcd63c5218041c
Andy-Python-Programmer <Andy-Python-Programmer@users.noreply.github.com>
2022-01-18 05:01:37 +0000

    deploy: 419719cc86e186d3c6888fe0e174517cf03b0202

--- aero_kernel/syscall/fs/sidebar-items.js
+++ aero_kernel/syscall/fs/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["access",""],["chdir",""],["close",""],["dup",""],["fcntl",""],["getcwd",""],["getdents",""],["ioctl",""],["mkdir",""],["mkdirat",""],["open",""],["pipe",""],["read",""],["rmdir",""],["seek",""],["unlink",""],["write",""]]});
+initSidebarItems({"fn":[["access",""],["chdir",""],["close",""],["dup",""],["dup2",""],["fcntl",""],["getcwd",""],["getdents",""],["ioctl",""],["mkdir",""],["mkdirat",""],["open",""],["pipe",""],["read",""],["rmdir",""],["seek",""],["unlink",""],["write",""]]});
