0f0461f906873f432c3efde774a087128e7d3651
Andy-Python-Programmer <Andy-Python-Programmer@users.noreply.github.com>
2022-02-03 06:55:02 +0000

    deploy: 844061c779c04be50f2afabd22be41441250ac36

--- aero_kernel/mem/alloc/sidebar-items.js
+++ aero_kernel/mem/alloc/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["HEAP_END",""],["HEAP_MAX_SIZE",""],["HEAP_START",""]],"fn":[["alloc_error_handler",""],["init_heap","Initialize the heap at the [HEAP_START]."]],"struct":[["LockedHeap",""]]});
+initSidebarItems({"constant":[["HEAP_END",""],["HEAP_MAX_SIZE",""],["HEAP_START",""]],"fn":[["alloc_error_handler",""],["init_heap","Initialize the heap at the [HEAP_START]."]],"struct":[["Allocator",""],["LockedHeap",""],["ProtectedAllocator",""],["Slab","The slab is the primary unit of currency in the slab allocator."],["SlabHeader",""]]});
